8a5ffce8f2e9e5a7506951b7b766414ea5851b29
zs <zs@bjpowernode.com>
2021-07-19 15:08:15 +0800

    test01导入

---
+++ src/main/java/com/domain/user.java
@@ -0,0 +1,17 @@
+package com.domain;
+
+public class user {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public user(int age) {
+        this.age = age;
+    }
+}
